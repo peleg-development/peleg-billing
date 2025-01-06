7cf82dc46a1251fae5e786f7ef4455527c341908
KurdY <liadpeleg03@gmail.com>
2025-01-06 21:30:55 +0200

    Update Server.lua

--- Server/Server.lua
+++ Server/Server.lua
@@ -224,7 +224,7 @@ QBCore.Functions.CreateCallback('krs-billing:getPlayerName', function(source, cb
     if targetPlayer then
         local charInfo = targetPlayer.PlayerData.charinfo
         local fullName = ("%s %s"):format(charInfo.firstname, charInfo.lastname)
-        local cid = targetPlayer.citizenid 
+        local cid = targetPlayer.cid 
         print(string.format("[peleg-billing] Retrieved name for serverId %d: %s | CID: %s", serverId, fullName, cid))
         cb({ name = fullName, cid = cid })
     else
