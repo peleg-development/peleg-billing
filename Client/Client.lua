29cbaa925825150486a6ad31ffe503cbcce780cf
KurdY <liadpeleg03@gmail.com>
2025-01-08 22:07:28 +0200

    Update Client.lua

--- Client/Client.lua
+++ Client/Client.lua
@@ -48,6 +48,7 @@ RegisterNUICallback('krs-billing:callback:close', function(data, cb)
 end)
 
 RegisterNUICallback('krs-billing:callback:billPlayer', function(data, cb)
+    print(data.cid)
     TriggerServerEvent('krs-billing:server:billPlayer', data)
     cb("ok")
 end)
