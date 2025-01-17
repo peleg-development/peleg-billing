7a62197359eaede043295278b93d34e9d0beae56
KurdY <liadpeleg03@gmail.com>
2025-01-17 21:30:22 +0200

    Update Sh_Config.lua

--- Shared/Sh_Config.lua
+++ Shared/Sh_Config.lua
@@ -9,6 +9,21 @@ Config.Framework = "QB" -- Options: "QB" or "ESX"
 Config.BillingItem = "" -- Define the item here; leave empty for no item
 Config.BillCommand = "bill" -- Define the command to open the billing menu
 
+-- __   _  _____  _______ _____ _______ __   __
+-- | \  | |     |    |      |   |______   \_/  
+-- |  \_| |_____|    |    __|__ |          |   
+
+Config.Notify = "qb" -- Options: "esx", "qb", "okokNotify", "peleg-notify"
+
+-- _______ _______ _     _ _____ __   _  ______
+--    |    |_____|  \___/    |   | \  | |  ____
+--    |    |     | _/   \_ __|__ |  \_| |_____|
+
+Config.Tax = {
+    Enabled = false, -- Enable or disable the tax system
+    Percentage = 0 -- Set the tax percentage (e.g., 10%)
+}
+
 -- _____  _____  ______  _______
 --   |   |     | |_____] |______
 -- __|   |_____| |_____] ______|
@@ -26,23 +41,9 @@ Config.Jobs = {
     },
 }
 
--- __   _  _____  _______ _____ _______ __   __
--- | \  | |     |    |      |   |______   \_/  
--- |  \_| |_____|    |    __|__ |          |   
-
-Config.Notify = "qb" -- Options: "esx", "qb", "okokNotify", "peleg-notify"
-
 --         _____  _______ _______        _______
 -- |      |     | |       |_____| |      |______
 -- |_____ |_____| |_____  |     | |_____ |______
 
 Config.Locale = "en" -- Default to English; available options: "en", "he", "fr", "it"
 
--- _______ _______ _     _ _____ __   _  ______
---    |    |_____|  \___/    |   | \  | |  ____
---    |    |     | _/   \_ __|__ |  \_| |_____|
-
-Config.Tax = {
-    Enabled = false, -- Enable or disable the tax system
-    Percentage = 0 -- Set the tax percentage (e.g., 10%)
-}
