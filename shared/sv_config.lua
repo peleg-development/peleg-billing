7ea0a13fd0433e8f6a430c4873e9f7719ddd9973
Peleg <106535297+peleg-development@users.noreply.github.com>
2025-08-31 18:27:56 +0300

    removed logs webhooks...

--- shared/sv_config.lua
+++ shared/sv_config.lua
@@ -6,10 +6,10 @@ SVConfig = SVConfig or {}
 SVConfig.LogsEnabled = true
 
 SVConfig.Webhooks = {
-	createBill = 'https://discord.com/api/webhooks/1409940404874252309/Y2lHEaYGXvgzjzfC8zNC-bADoib8P46iTBKVrBlyBtf0D0zU0tVmRgaPowTgbRbpp7z7',
-	payBill = 'https://discord.com/api/webhooks/1409940404874252309/Y2lHEaYGXvgzjzfC8zNC-bADoib8P46iTBKVrBlyBtf0D0zU0tVmRgaPowTgbRbpp7z7',
-	refundBill = 'https://discord.com/api/webhooks/1409940404874252309/Y2lHEaYGXvgzjzfC8zNC-bADoib8P46iTBKVrBlyBtf0D0zU0tVmRgaPowTgbRbpp7z7',
-	setGradePerm = 'https://discord.com/api/webhooks/1409940404874252309/Y2lHEaYGXvgzjzfC8zNC-bADoib8P46iTBKVrBlyBtf0D0zU0tVmRgaPowTgbRbpp7z7'
+	createBill = '',
+	payBill = '',
+	refundBill = '',
+	setGradePerm = ''
 }
 
 
